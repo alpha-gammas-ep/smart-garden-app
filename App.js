72bfd00cd8cffb3e9030c0bdef6baf1a5bf519c5
bhuang1323 <brendahuang@berkeley.edu>
2021-03-28 00:39:39 -0700

    Background Issue Fixed

--- App.js
+++ App.js
@@ -7,15 +7,17 @@ import WelcomeScreen from './components/WelcomeScreen.js';
 import Tabs from './components/Tabs.js';
 import Home from './components/Home.js';
 import Settings from './components/Settings.js';
+import Logo from './components/Logo.js'
+
 
 const Stack = createStackNavigator();
 
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="WelcomeScreen" component={WelcomeScreen}/>
-                <Stack.Screen name="Home" component={Tabs} />
+            <Stack.Navigator screenOptions={{headerLeft: () => <Logo />}}>
+                <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} options={{headerShown: false}}/>
+                <Stack.Screen name="Home" component={Tabs} options={{title:'Smart Garden', headerTitleStyle: {color:'#669850', fontSize: 20, fontWeight: 'bold'}, headerTitleAlign: 'left', headerTitleContainerStyle: {left: 55}}}/>
             </Stack.Navigator>
         </NavigationContainer>
     );
